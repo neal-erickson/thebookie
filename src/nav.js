c2cf1f84f86d1f4d20f7fb2bcdda3367cf8cfd7f
Neal Erickson <nerickson@homeaway.com>
2013-07-05 12:38:56 -0600

    Functional prototype going using number keys and actually traversing tree

--- src/nav.js
+++ src/nav.js
@@ -1,17 +1,59 @@
+ko.extenders.logChange = function(target, option) {
+    target.subscribe(function(newValue) {
+       console.log(option + ": " + newValue);
+    });
+    return target;
+};
+
 // Initialize the KO view model
 var vm = { };
 
-vm.rootNode = ko.observable(null);
-vm.rootNodeJSON = ko.computed(function(){
-    return JSON.stringify(rootNode);
-});
+vm.tree = ko.observable(null);
 
+vm.activeNodes = ko.observableArray([]).extend({ logChange: 'nodes' });
+
+// Note
+vm.setActiveNodes = function(node){
+    var testIndex = 1;
+    ko.utils.arrayForEach(node.children, function(childNode){
+        childNode.assignedHotkey = testIndex;
+        testIndex++;
+    })
+    vm.activeNodes(node.children);
+};
+
+vm.selectNode = function(node){
+    if(node.url){ // If this is a link, follow it
+        chrome.tabs.create({ url: node.url })
+    } else { // folder
+        vm.setActiveNodes(node);
+    }
+};
+
+vm.reset = function(){
+    vm.setActiveNodes(vm.tree());
+};
+
+vm.hotkey = function(data, event){
+    console.log(event);
+
+    var index = parseInt(event.keyCode) - 48;
+    var match = ko.utils.arrayFirst(vm.activeNodes(), function(node){
+        return node.assignedHotkey === index;
+    });
+    if(match != null){
+        vm.selectNode(match);
+    }
+};
+
+// Load the data from chrome
 chrome.bookmarks.getTree(function(tree){
-    vm.rootNode(tree);
-})
+    vm.tree(tree[0]);
+    vm.setActiveNodes(tree[0]);
+    console.log(tree[0]);
+});
 
 // This is the non-jquery way to be ready for something
 document.addEventListener('DOMContentLoaded', function () {
-  console.log('DOMContentLoaded');
   ko.applyBindings(vm);
 });
