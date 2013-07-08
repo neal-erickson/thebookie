548f23cec8bce4e5cb9898d892ea6177ab2aed67
Neal Erickson <minus.twelve@gmail.com>
2013-07-07 22:30:21 -0600

    breadcrumbs bar now works; more variables computed

--- src/nav.js
+++ src/nav.js
@@ -5,91 +5,82 @@ ko.extenders.logChange = function(target, option) {
     return target;
 };
 
-// Initialize the KO view model
-var vm = { };
-
-vm.tree = ko.observable(null);
-
-vm.activeNodes = ko.observableArray([]).extend({ logChange: 'nodes' });
-
 // Current hotkey plan is 12 keys, 3 rows of 4
 var alphaHotkeys = ['Q', 'W', 'E', 'R', 'A', 'S', 'D', 'F', 'Z', 'X', 'C', 'V'];
 
-// Note
-vm.setActiveNodes = function(node){
-    for(var i=0; i<12; i++){
-        // todo
+// Initialize the KO view model
+var vm = { };
+vm.tree = ko.observable(null);
+vm.rootNode = ko.observable(null);
+vm.selectedNode = ko.observable();
 
+//vm.activeNodes = ko.observableArray([]).extend({ logChange: 'nodes' });
+vm.activeNodes = ko.computed(function(){
+    if(!vm.selectedNode()) return [];
+
+    var nodes = vm.selectedNode().children;
+    for(var index in nodes){
+        nodes[index].assignedHotkey = alphaHotkeys[index];
     }
-    var index = 0;
-    ko.utils.arrayForEach(node.children, function(childNode){
-        childNode.assignedHotkey = alphaHotkeys[index];
-        index++;
-    })
-    vm.activeNodes(node.children);
-};
+    return nodes;
+});
 
-vm.selectNode = function(node){
-    if(node.url){ // If this is a link, follow it
+vm.nodeClicked = function(node){
+    if(node.url){
         chrome.tabs.create({ url: node.url });
         // Add this back in to make it close itself.
-        // chrome.tabs.getCurrent(function(tab) {
-        //     chrome.tabs.remove(tab.id);
-        // });
-    } else { // folder
-        vm.setActiveNodes(node);
+        // chrome.tabs.getCurrent(function(tab) {chrome.tabs.remove(tab.id); });
+    } else { 
+        //vm.nodeHistory.push(node);
+        vm.selectedNode(node);
     }
 };
 
 vm.reset = function(){
-    vm.setActiveNodes(vm.tree());
+    vm.selectedNode(vm.rootNode());
 };
 
-vm.levels = ko.observableArray();
-
 vm.breadcrumbs = ko.computed(function(){
-    var crumbs = [{
-        clickFunction: vm.reset,
-        name: 'Root'
-    }];
-    ko.utils.arrayForEach(vm.levels(), function(item){
-        crumbs.push({ 
-            clickFunction: alert('!'),
-            name: item.title  
-        });
+    if(!vm.selectedNode()) return null;
+
+    var node = vm.selectedNode();
+    var crumbs = [];
+   
+    // Add 'dead' crumb for current
+    crumbs.push({
+        data: null,
+        clickFunction: null,
+        name: node.title
     });
-    return crumbs;
+
+    // iterate upwards through node parents
+    while(node.parentNode){
+        crumbs.push({
+            data: node.parentNode,
+            clickFunction: function() {
+                vm.selectedNode(this.data);
+            },
+            name: node.parentNode.title
+        });
+        node = node.parentNode;
+    }
+
+    return crumbs.reverse();
 });
 
 function goUpHierarchy(){
     // TODO: Do this
-    // var firstNode = vm.activeNodes()[0];
-    // if(firstNode){
-    //     var secondNode = firstNode.parent
-    // }
 }
 
 function handleHotkey(keyCode, keyValue){
-    // Number key checks
-    // if(!isNaN(keyValue)){
-    //     var match = ko.utils.arrayFirst(vm.activeNodes(), function(node){
-    //         return node.assignedHotkey === keyValue;
-    //     });
-
-    //     if(match != null){
-    //         vm.selectNode(match);
-    //     }
-    //     return;
-    // }
-
-    // letter keys
+    // alphabetic keys
     if(keyCode >= 65 && keyCode <= 90){
         var match = ko.utils.arrayFirst(vm.activeNodes(), function(node){
             return node.assignedHotkey === keyValue;
         });
 
         if(match != null){
-            vm.selectNode(match);
+            vm.nodeClicked(match);
         }
         return;
     }
@@ -111,11 +102,28 @@ vm.hotkey = function(data, event){
     handleHotkey(event.keyCode, keyValue);
 };
 
+function prepareBookmarkNodeRecursive(node, parent){
+    if(parent !== null){
+        node.parentNode = parent;
+    }
+
+    if(node.hasOwnProperty('children')){
+        ko.utils.arrayForEach(node.children, function(child) {
+            prepareBookmarkNodeRecursive(child, node);
+        });
+    }
+}
+
 // Load the data from chrome.bookmarks
 chrome.bookmarks.getTree(function(tree){
     var rootNode = tree[0].children[0]; // this is the 'bookmarks bar'
+    rootNode.parentId = null;
+
+    prepareBookmarkNodeRecursive(rootNode, null);
+
     vm.tree(rootNode);
-    vm.setActiveNodes(rootNode);
+    vm.rootNode(rootNode);
+    vm.selectedNode(rootNode);
 });
 
 // This is the non-jquery way to be ready for something
